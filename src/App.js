0493e68f026b83b73f67bb8a86bdaca072343095
JJ Escandor <89546590+jjescandor@users.noreply.github.com>
2022-04-03 17:27:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import BestBooks from './BestBooks';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    }
+  }
+
+  loginHandler = (user) => {
+    this.setState({
+      user,
+    })
+  }
+
+  logoutHandler = () => {
+    this.setState({
+      user: null,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <Switch>
+            <Route exact path="/">
+              {/* PLACEHOLDER: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              <BestBooks />
+            </Route>
+            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+        {/* PLACEHOLDER: render a navigation link to the about page */}
+      </Navbar>
+    )
+  }
+}
+
+export default Header;

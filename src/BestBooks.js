55d2bb0110223d6eabee10a62c22b1ba1d2a4121
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-03 18:29:49 -0400

    added carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -33,19 +33,20 @@ class BestBooks extends React.Component {
         <h2>Available Books</h2>
         {this.state.books.length ? (
           this.props.books.map((value, idx) => {
-            <Carousel key={idx}>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={value.poster}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+            return (
+              <Carousel key={idx}>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={value.poster}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>)
           })
         ) : (
           <h3>No Books Found :(</h3>

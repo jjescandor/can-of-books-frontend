29e2d651cb97769e295e2abe171c469d39157ecf
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-04-12 20:25:31 -0500

    conditionally rendered logout button

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import { IoLogOutSharp } from 'react-icons/io5';
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+  const { logout, isAuthenticated } = useAuth0();
 
   //   return (
   //     <button onClick={() => logout({ returnTo: window.location.origin })}>
@@ -10,12 +10,14 @@ const LogoutButton = () => {
   //     </button>
   //   );
   return (
-    <div>
-      <h6 onClick={() => logout({ returnTo: window.location.origin })}>
-        <IoLogOutSharp />
-        &nbsp;&nbsp; Log Out
-      </h6>
-    </div>
+    isAuthenticated && (
+      <div>
+        <h6 onClick={() => logout({ returnTo: window.location.origin })}>
+          <IoLogOutSharp />
+          &nbsp;&nbsp; Log Out
+        </h6>
+      </div>
+    )
   );
 };
 

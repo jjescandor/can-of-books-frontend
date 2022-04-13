57a1490c2f42862a05cd59a3915e2818e90ae912
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-04-12 19:50:04 -0500

    added auth0

---
+++ src/LogoutButton.js
@@ -0,0 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import { IoLogOutSharp } from 'react-icons/io5';
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  //   return (
+  //     <button onClick={() => logout({ returnTo: window.location.origin })}>
+  //       Log Out
+  //     </button>
+  //   );
+  return (
+    <div>
+      <h6 onClick={() => logout({ returnTo: window.location.origin })}>
+        <IoLogOutSharp />
+        &nbsp;&nbsp; Log Out
+      </h6>
+    </div>
+  );
+};
+
+export default LogoutButton;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Index.css';
 import App from './App.js';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+  <Auth0Provider
+    domain='dev-4wdokf-s.us.auth0.com'
+    clientId='Y4QBRzzTZ3x5DVSdETjP1UVHVNefAFR5'
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
 );

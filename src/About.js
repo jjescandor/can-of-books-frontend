c738005ae8975b258d356dd7b48a9e572ea268f3
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-15 14:02:22 -0400

    fixed About.js

--- src/About.js
+++ src/About.js
@@ -15,26 +15,29 @@ class Profile extends Component {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
     return (
-      <div className='developers'>
-        <Card className='developersCard'>
-          <Card.Body className='developersBody'>
-            <Card.Img className='img' src='https://res.cloudinary.com/dxg5jg10h/image/upload/v1629765279/profile_wdlyay.svg' alt='' />
-            <Card.Title>Erich Hartnauer</Card.Title>
-            <Card.Text>
-              I'm Erich Hartnauer. I am a boot Full-Stack developer who thoroughly enjoys making new and exciting practical applications. I am new to the tech field and looking to learn and grow with a company to sharpen my skills.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      <div>
+        <h1>About Us</h1>
+        <div className='developers'>
+          <Card className='developersCard'>
+            <Card.Body className='developersBody'>
+              <Card.Img className='img' src='https://res.cloudinary.com/dxg5jg10h/image/upload/v1629765279/profile_wdlyay.svg' alt='' />
+              <Card.Title>Erich Hartnauer</Card.Title>
+              <Card.Text>
+                I am a Full-Stack developer who thoroughly enjoys making new and exciting practical applications. <br />I am new to the tech field and looking to learn and grow with a company to sharpen my skills.
+              </Card.Text>
+            </Card.Body>
+          </Card>
 
-        <Card className='developersCard'>
-          <Card.Body className='developersBody'>
-            <Card.Img className='img' src='https://res.cloudinary.com/dxg5jg10h/image/upload/v1629765279/profile_wdlyay.svg' alt='' />
-            <Card.Title>JJ Escandor</Card.Title>
-            <Card.Text>
-              B Dope af
-            </Card.Text>
-          </Card.Body>
-        </Card>
+          <Card className='developersCard'>
+            <Card.Body className='developersBody'>
+              <Card.Img className='img' src='https://res.cloudinary.com/dxg5jg10h/image/upload/v1629765279/profile_wdlyay.svg' alt='' />
+              <Card.Title>JJ Escandor</Card.Title>
+              <Card.Text>
+                I'm a Data Scientist with background in software engineering and defense consulting leveraging over a decade of technical and consulting experience. Passionate about using code to optimize efficiency while developing creative, customer-focused solutions. Veteran of the United States Navy, with active Secret clearance.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>)
   }
 };

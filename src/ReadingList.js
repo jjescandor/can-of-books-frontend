643f0aa69d38269815fd01f7691e9d80f198eed2
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-14 21:04:51 -0400

    added styling

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -3,6 +3,8 @@ import "./ReadingList.css"
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import ReactStars from 'react-rating-stars-component';
 
 class ReadingList extends React.Component {
     constructor(props) {
@@ -65,20 +67,41 @@ class ReadingList extends React.Component {
         return (
             <>
                 <h1>My Reading List</h1>
-                {this.state.readingList.map(book => {
-                    return (<div div id="readingListDiv" key={book._id} >
-                        <h3>{book.title}</h3>
-                        <img src={book.url} alt="" />
-                        <Button
-                            id="readingListBton"
-                            onClick={() => this.handleDeleteBook(book)}
-                        >
-                            Delete
-                        </Button>
-                    </div>)
-                })}
-            </>
-        )
+                <Accordion
+                    id="readingListCont"
+                    defaultActiveKey='0'
+                    flush>
+                    {this.state.readingList.map(book =>
+                        <Accordion.Item
+
+                            eventKey={book._id}>
+                            <Accordion.Header id="readingListHdr">{book.title}</Accordion.Header>
+                            <Accordion.Body id="readingListBody">
+                                <img
+                                    id="readingListImg"
+                                    src={book.url}
+                                    alt="" />
+                                <div>
+                                    <p>Genre: {book.genre}</p>
+                                    <ReactStars
+                                        value={4.5}
+                                        count={5}
+                                        size={24}
+                                        activeColor='#F7D004'
+                                    />
+                                    <p>{book.description}</p>
+                                    <Button
+                                        id="readingListBtn"
+                                        onClick={() => this.handleDeleteBook(book)}>
+                                        Delete
+                                    </Button>
+                                </div>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    )}
+                </Accordion>
+            </>)
+
     }
 }
 
